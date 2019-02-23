d3b077a6553821fac57cab90cd487550eb96ec29
DeadLikado <46147850+DeadLikado01@users.noreply.github.com>
2019-02-22 20:56:24 -0600

    Create util.php

---
+++ util.php
@@ -0,0 +1,43 @@
+<?php
+$rhversion = "3.0.1";
+$white = "\e[97m";
+$black = "\e[30m\e[1m";
+$yellow = "\e[93m";
+$orange = "\e[38;5;208m";
+$blue   = "\e[34m";
+$lblue  = "\e[36m";
+$cln    = "\e[0m";
+$green  = "\e[92m";
+$fgreen = "\e[32m";
+$red    = "\e[91m";
+$magenta = "\e[35m";
+$bluebg = "\e[44m";
+$lbluebg = "\e[106m";
+$greenbg = "\e[42m";
+$lgreenbg = "\e[102m";
+$yellowbg = "\e[43m";
+$lyellowbg = "\e[103m";
+$redbg = "\e[101m";
+$grey = "\e[37m";
+$cyan = "\e[36m";
+$bold   = "\e[1m";
+function hackmaster_banner(){
+	echo "\e[38;5;208m   Una Herramienta Completa Para\e[93m Recoleccionar Información\e[38;5;208m Y\e[36m Escanear Vulneravilidades!!!\e[92m  
+	- - - ☆- - - - - - •• - - - - - - ☆ •
+    ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀
+   ▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒
+   ▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░
+   ░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄
+   ░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄
+   ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒
+   ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░
+   ░  ░░ ░  ░   ▒   ░        ░ ░░ ░
+   ░  ░  ░      ░  ░░ ░      ░  ░
+                  ░     \e[93m Ver  3.1.0\e[91m  
+                                                             
+         {C} Hecho Por- DeadLikado Hack         
+         +-+\e[97m [DL] The HACKING ;)
+\e[32m
+  \n";
+}
+?>
